252f90621853eb4866e7d36d42806b105ca29dce
yohang <yohan@giarel.li>
2016-09-19 11:32:54 +0200

    Updated docker env for windows

--- reset.sh
+++ reset.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker-compose run --entrypoint=php -d cli bin/console doctrine:schema:drop --force
-docker-compose run --entrypoint=php -d cli bin/console doctrine:schema:create
-docker-compose run --entrypoint=php -d cli bin/console hautelook_alice:doctrine:fixtures:load --no-interaction
+docker-compose run --entrypoint=php cli bin/console doctrine:schema:drop --force
+docker-compose run --entrypoint=php cli bin/console doctrine:schema:create
+docker-compose run --entrypoint=php cli bin/console hautelook_alice:doctrine:fixtures:load --no-interaction
